c0f5ce0aff460b8cac03cae69818d92d26e5c029
TravlingMonk <45330301+ErrorBox@users.noreply.github.com>
2021-09-06 20:23:42 -0500

    Adding the clicker functions

---
+++ js/clicker.js
@@ -0,0 +1,45 @@
+function autocook(){
+    var intervalId = window.setInterval(function(){
+        var cook = localStorage.getItem("cookie")
+        var math = parseFloat(cook) + 1; /* add buildings and multiplyers here */
+        localStorage.setItem("cookie",math)   },1000)
+}
+function textupdate(){
+    var intervalId = window.setInterval(function(){
+        $("#cookietext").text("cookies: " + localStorage.getItem("cookie"))
+    },10)
+}
+function firsttime(){
+    $(document).ready(function(){
+        if(localStorage.getItem("cookie") != null) {
+            console.log("data found")
+        }
+        else{
+            console.log("No local data found")
+            console.log("adding variables")
+            localStorage.setItem("cookie",0)
+        }
+    })
+}
+function clickcook(){
+    $("#bigbut").click(function(){
+        var cookie = localStorage.getItem("cookie")
+        var math = parseFloat(cookie) + 1;
+        localStorage.setItem("cookie",math)
+    })
+}
+function resdetect() {
+    var intervalId = window.setInterval(function(){
+        var screenX = window.innerHeight
+        var screenY = window.innerWidth
+        var str = "X: " + screenX + " Y: " + screenY;
+        $("#resolution").text(str)
+
+        var textwidth = $("#cookietext").width() / 2
+        $("#cookietext").css( { "margin-left" : -textwidth })
+   },10)
+}
+function cheat(){
+    /* might be able to have multiple cheats
+     in one func otherwise make a cheat.js */
+}
